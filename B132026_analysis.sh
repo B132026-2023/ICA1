70fd7cacf268ce37ef04e2df4a43d4079b7ba10d
B132026-2023 <s1816717@ed.ac.uk>
2023-10-16 18:35:07 +0100

    Successfully aligned fastq sequences with the reference genome

--- B132026_analysis.sh
+++ B132026_analysis.sh
@@ -7,7 +7,26 @@ mkdir -p B132026_results
 SEQPATH=/localdisk/data/BPSM/ICA1/fastq/
 
 #PATH TO REFERENCE GENOME (CHANGE AS REQUIRED)
-REFPATH=/localdisk/data/BPSM/ICA1/Tcongo_genome/
+REFGEN=/localdisk/data/BPSM/ICA1/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz
 
 #GENERATE FASTQC REPORTS
-fastqc ${SEQPATH}/*.fq.gz -o ./B132026_results/ 
+#fastqc ${SEQPATH}/*.fq.gz -o ./B132026_results/ -t 6
+
+#ALIGNMENT OF FASTQ READS WITH THE REFERENCE GENOME 
+bowtie2-build ${REFGEN} ./B132026_results/reference --threads 64
+
+for sequence in ${SEQPATH}*_1.fq.gz; do
+
+    sample_name=$(echo $sequence|cut -d"_" -f 1)
+
+    bowtie2 -x ./B132026_results/reference \
+            -1 ${sample_name}_1.fq.gz -2 ${sample_name}_2.fq.gz \
+            -S ./B132026_results/$(basename ${sample_name}).sam \
+            --threads 64
+
+    samtools view -bS ./B132026_results/$(basename ${sample_name}).sam > \
+            ./B132026_results/$(basename ${sample_name}).bam \
+            -@ 64
+
+    rm *.sam
+done
